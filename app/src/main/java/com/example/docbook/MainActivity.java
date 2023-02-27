0426981fe1d4601b4316612453888776574ff941
Vinayak Prakash <vinayakprakash2121@gmail.com>
2023-02-27 17:55:32 +0530

    gi

--- app/src/main/java/com/example/docbook/MainActivity.java
+++ app/src/main/java/com/example/docbook/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.docbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+EditText uname,pass;
+Button b1;
+TextView t1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        uname=findViewById(R.id.username);
+        pass=findViewById(R.id.password);
+        b1=findViewById(R.id.b1);
+    b1.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String username=uname.getText().toString();
+            String password=pass.getText().toString();
+
+        }
+    });
     }
+
 }
