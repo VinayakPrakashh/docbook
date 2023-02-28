56b64cfb2e07f0e1d76f424f64976f8b19a90c56
Vinayak Prakash <vinayakprakash2121@gmail.com>
2023-02-28 19:36:40 +0530

    girr

--- app/src/main/java/com/example/docbook/RegisterActivity.java
+++ app/src/main/java/com/example/docbook/RegisterActivity.java
@@ -1,13 +1,52 @@
 package com.example.docbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
-
+Button b1;
+EditText uname,pass,mail;
+TextView login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        uname=findViewById(R.id.username);
+        pass=findViewById(R.id.password);
+        b1=findViewById(R.id.b1);
+        mail=findViewById(R.id.mail);
+        login=findViewById(R.id.tregister);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this,MainActivity.class));
+            }
+        });
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username=uname.getText().toString();
+                String email=mail.getText().toString();
+                String password=pass.getText().toString();
+                Database db=new Database(getApplicationContext(),"healthcare",null,1);
+                if(username.length()==0 || email.length()==0 || password.length()==0){
+                    Toast.makeText(RegisterActivity.this, "Please fill the Required fields", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    db.register(username,email,password);
+                    Toast.makeText(RegisterActivity.this, "Registration success", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(RegisterActivity.this,MainActivity.class));
+                }
+            }
+        });
     }
+
+
 }
