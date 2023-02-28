aebcf3e3e067f597bd8757868d347e041b51476b
VinayakPrakashh <vinayakprakash2121@gmail.com>
2023-02-28 15:07:20 +0530

    fini

---
+++ app/src/main/java/com/example/docbook/Database.java
@@ -0,0 +1,31 @@
+package com.example.docbook;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Database extends SQLiteOpenHelper {
+
+    public Database(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    String qryl="create table users(username text,email text,password text)";
+    sqLiteDatabase.execSQL(qryl);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+    public void register(String username,String email,String password){
+        ContentValues cv=new ContentValues();
+        cv.put("username",username);
+
+    }
+}
