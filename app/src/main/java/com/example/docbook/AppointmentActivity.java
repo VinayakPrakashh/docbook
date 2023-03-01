dc42149b7e0c5553afd8143e33e12569a1038836
VinayakPrakashh <vinayakprakash2121@gmail.com>
2023-03-01 15:35:56 +0530

    fini

--- app/src/main/java/com/example/docbook/AppointmentActivity.java
+++ app/src/main/java/com/example/docbook/AppointmentActivity.java
@@ -7,8 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class AppointmentActivity extends AppCompatActivity {
 Button appointment;
+FirebaseAuth mAuth;
+    FirebaseUser mUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
