0297f9cad222df72487aa9c217a6bd4ba7a5ec6e
Vinayak Prakash <vinayakprakash2121@gmail.com>
2023-03-04 19:30:56 +0530

    girr

--- app/src/main/java/com/example/docbook/DoctorDetailsActivity.java
+++ app/src/main/java/com/example/docbook/DoctorDetailsActivity.java
@@ -2,20 +2,22 @@ package com.example.docbook;
 
 import static android.content.ContentValues.TAG;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -23,7 +25,7 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 public class DoctorDetailsActivity extends AppCompatActivity {
-TextView docname2,docspec2,dochospital2,dexp2,doccontact2;
+TextView docname2,docspec2,dochospital2,dexp2,doccontact2,dquali;
 Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +40,34 @@ Button b1;
         dochospital2=findViewById(R.id.dhospital);
         dexp2=findViewById(R.id.dexp);
         doccontact2=findViewById(R.id.dcontact);
+        dquali=findViewById(R.id.dqualification);
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 b1=findViewById(R.id.book_appointment_button);
+        // Get a reference to the Firebase Storage instance
         FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageRef = storage.getReference();
-        StorageReference imageRef = storageRef.child("cardiologists.jpg");
-        ImageView imageView = findViewById(R.id.dphoto);
-        Glide.with(this /* context */)
-                .load(imageRef)
-                .into(imageView);
+
+// Create a reference to the image file you want to download
+        StorageReference imageRef = storage.getReference().child("cardiologists.jpg");
+
+// Download the image file into a byte array
+        imageRef.getBytes(Long.MAX_VALUE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                // Convert the byte array into a Bitmap object
+                Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+
+                // Set the Bitmap object in an ImageView
+                ImageView imageView = findViewById(R.id.dphoto);
+                imageView.setImageBitmap(bmp);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors that occur during the download
+                Log.e("TAG", "Failed to download image", exception);
+            }
+        });
+
 
         db.collection("doctor")
                 .document("neurologist")
@@ -65,6 +86,7 @@ b1=findViewById(R.id.book_appointment_button);
                                 int experience = document.getLong("experience").intValue();
                                 String hospital = document.getString("hospital");
                                 long contact = document.getLong("contact");
+                                String qualification=document.getString("qualification");
                                 String exp = Integer.toString(experience);
                                 // Update UI with doctor details
                                 String dcon= Long.toString(contact);
@@ -77,6 +99,7 @@ b1=findViewById(R.id.book_appointment_button);
                                 dochospital2.setText("Hospital: "+hospital);
                                 dexp2.setText("Experience: "+exp+" Years");
                                 doccontact2.setText("Contact No: "+dcon);
+                                dquali.setText("Qualification: "+qualification);
 
 
                             } else {
