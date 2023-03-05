f21ae8b93e83c016d04f295ba199e8b58065a2bf
Vinayak Prakash <vinayakprakash2121@gmail.com>
2023-03-05 14:17:54 +0530

    girr

--- app/src/main/java/com/example/docbook/MedicineActivity.java
+++ app/src/main/java/com/example/docbook/MedicineActivity.java
@@ -1,14 +1,66 @@
 package com.example.docbook;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.docbook.ProductAdapter.Product;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MedicineActivity extends AppCompatActivity {
-
+    private RecyclerView recyclerView;
+    private ProductAdapter productAdapter;
+    private List<ProductAdapter.Product> productList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medicine);
-    }
-}
+
+
+                recyclerView = findViewById(R.id.recyclerView);
+                recyclerView.setHasFixedSize(true);
+                recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+                productList = new ArrayList<>();
+                productAdapter = new ProductAdapter(this, productList);
+                recyclerView.setAdapter(productAdapter);
+
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                db.collection("products")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        String name = document.getString("name");
+                                        long price = document.getLong("price");
+                                        int price2 = Math.toIntExact(price);
+
+                                        ProductAdapter.Product product = new Product(name, price2);
+                                        productList.add(product);
+                                    }
+                                    productAdapter.notifyDataSetChanged();
+                                } else {
+                                    Log.d(TAG, "Error getting documents: ", task.getException());
+                                }
+                            }
+                        });
+            }
+        }
+
+
---
+++ app/src/main/java/com/example/docbook/ProductAdapter.java
@@ -0,0 +1,72 @@
+package com.example.docbook;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+    private Context context;
+    private List<Product> productList;
+
+    public ProductAdapter(Context context, List<Product> productList) {
+        this.context = context;
+        this.productList = productList;
+    }
+
+    @NonNull
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.activity_medicine, null);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
+        Product product = productList.get(position);
+        holder.textViewProductName.setText(product.getName());
+        holder.textViewProductPrice.setText(String.format("$%.2f", product.getPrice()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    public static class ProductViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewProductName, textViewProductPrice;
+
+        public ProductViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewProductName = itemView.findViewById(R.id.product_name);
+            textViewProductPrice = itemView.findViewById(R.id.product_price);
+        }
+    }
+    public static class Product {
+        private String name;
+        private int price;
+
+        public Product(String name, int price) {
+            this.name = name;
+            this.price = price;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getPrice() {
+            return price;
+        }
+    }
+
+}
+
