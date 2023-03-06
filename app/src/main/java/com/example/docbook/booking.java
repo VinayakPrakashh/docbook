96ead757025610f8e96595b754963241c4cda674
Vinayak Prakash <vinayakprakash2121@gmail.com>
2023-03-06 23:53:51 +0530

    girr

--- app/src/main/java/com/example/docbook/booking.java
+++ app/src/main/java/com/example/docbook/booking.java
@@ -1,10 +1,7 @@
 package com.example.docbook;
 
-import static com.example.docbook.Connection.isNetworkAvailable;
-
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -14,33 +11,42 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.material.snackbar.Snackbar;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class booking extends AppCompatActivity {
 EditText name,reasons,contact,ages;
+public int val;
     private String[] genderOptions = {"Male", "Female", "Other"};
-ImageButton date;
+    private String[] timeslots=
+            {"9 AM to 10 AM",
+            "10 AM to 11 AM",
+            "11 AM to 12 PM",
+            "2 PM to 3 PM",
+            "3 PM to 4 PM"};
+ImageButton date,timeslot;
 Button b1;
-TextView genders;
+TextView genders,timetext;
     int day;
     int month;
     int year;
@@ -56,15 +62,31 @@ contact=findViewById(R.id.contactnumber);
 ages=findViewById(R.id.age);
 b1=findViewById(R.id.book);
 genders=findViewById(R.id.gender);
-
-
+timeslot=findViewById(R.id.time);
+timetext=findViewById(R.id.timetex);
         datewrite2 = (TextView) findViewById(R.id.datetex);
         date = (ImageButton) findViewById(R.id.date);
         cal = Calendar.getInstance();
         day = cal.get(Calendar.DAY_OF_MONTH);
         month = cal.get(Calendar.MONTH);
         year = cal.get(Calendar.YEAR);
+       timeslot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(booking.this);
+                builder.setTitle("Select Time Slot");
+                builder.setSingleChoiceItems(timeslots, -1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
+                        String selectedtime = timeslots[which];
+                        timetext.setText(selectedtime);
+                        dialog.dismiss();
+                    }
+                });
+                builder.create().show();
+            }
+        });
         genders.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -91,66 +113,18 @@ genders=findViewById(R.id.gender);
                 int year = calendar.get(Calendar.YEAR);
                 int month = calendar.get(Calendar.MONTH);
                 int day = calendar.get(Calendar.DAY_OF_MONTH);
-                int hour = calendar.get(Calendar.HOUR_OF_DAY);
-                int minute = calendar.get(Calendar.MINUTE);
 
-// Add one day to the current date to set it as the minimum date
+                // Add one day to the current date to set it as the minimum date
                 calendar.add(Calendar.DAY_OF_MONTH, 1);
 
-// Show a DatePickerDialog to get the date
+                // Show a DatePickerDialog to get the date
                 DatePickerDialog datePickerDialog = new DatePickerDialog(
                         booking.this,
                         new DatePickerDialog.OnDateSetListener() {
                             @Override
                             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                                 // Update the EditText field with the selected date
-                                datewrite2.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
-
-                                // Show a TimePickerDialog to get the time
-                                TimePickerDialog timePickerDialog = new TimePickerDialog(
-                                        booking.this,
-                                        new TimePickerDialog.OnTimeSetListener() {
-                                            @Override
-                                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                                                // Get the current time
-                                                Calendar currentTime = Calendar.getInstance();
-
-                                                // Set the selected time to the current time if it is in the past
-                                                if (year == currentTime.get(Calendar.YEAR)
-                                                        && monthOfYear == currentTime.get(Calendar.MONTH)
-                                                        && dayOfMonth == currentTime.get(Calendar.DAY_OF_MONTH)
-                                                        && hourOfDay < currentTime.get(Calendar.HOUR_OF_DAY)) {
-                                                    hourOfDay = currentTime.get(Calendar.HOUR_OF_DAY);
-                                                    minute = currentTime.get(Calendar.MINUTE);
-                                                }
-
-                                                // Check if the selected time is within the allowed range (9 AM - 4 PM)
-                                                if (hourOfDay < 9 || (hourOfDay == 9 && minute < 0) || hourOfDay > 16) {
-                                                    // Show an error message and return
-                                                    Toast.makeText(booking.this, "Booking time must be between 9 AM and 4 PM", Toast.LENGTH_LONG).show();
-                                                    return;
-                                                }
-
-                                                // Format the selected time as 12-hour format
-                                                String timeFormat = hourOfDay >= 12 ? "PM" : "AM";
-                                                int hour = hourOfDay % 12;
-                                                hour = hour == 0 ? 12 : hour;
-                                                SimpleDateFormat timeFormatter = new SimpleDateFormat("h:mm a", Locale.getDefault());
-                                                String formattedTime = timeFormatter.format(new Date(0, 0, 0, hour, minute));
-
-                                                // Append the formatted time to the EditText field
-                                                datewrite2.append(" " + formattedTime);
-                                            }
-                                        },
-                                        hour,
-                                        minute,
-                                        false // Force time picker to display in 12-hour format
-                                );
-
-                                // Set the minimum time to the current time
-                                timePickerDialog.updateTime(hour, minute);
-
-                                timePickerDialog.show();
+                                datewrite2.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
                             }
                         },
                         year,
@@ -158,17 +132,14 @@ genders=findViewById(R.id.gender);
                         day
                 );
 
-// Set the minimum date to the next day
+                // Set the minimum date to the next day
                 datePickerDialog.getDatePicker().setMinDate(calendar.getTimeInMillis());
 
-// Show the DatePickerDialog
+                // Show the DatePickerDialog
                 datePickerDialog.show();
-
-
-
-
             }
         });
+
 
 
         b1.setOnClickListener(new View.OnClickListener() {
@@ -224,39 +195,28 @@ genders=findViewById(R.id.gender);
 
 
 
-                                // Validate gender
+                        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+                        try {
+                            Date date = format.parse(dateValue);
+                            Date now = new Date();
 
-                                // Validate date and time
-                                SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy h:mm a", Locale.getDefault());
-                                try {
-                                    Date date = format.parse(dateValue);
-                                    Date now = new Date();
+                            // Check if date is in the past
+                            if (date.before(now)) {
+                                datewrite2.setError("Please select a future date");
+                                datewrite2.requestFocus();
+                                return;
+                            }
 
-                                    // Check if date is in the past
-                                    if (date.before(now)) {
-                                        datewrite2.setError("Please select a future date and time");
-                                        datewrite2.requestFocus();
-                                        return;
-                                    }
+                            // Proceed with further checks or actions using the parsed date.
 
-                                    Calendar calendar = Calendar.getInstance();
-                                    calendar.setTime(date);
+                        } catch (ParseException e) {
+                            datewrite2.setError("Please select a valid date");
+                            datewrite2.requestFocus();
+                            return;
+                        }
 
-                                    // Check if time is between 9am and 4pm
-                                    int hour = calendar.get(Calendar.HOUR_OF_DAY);
-                                    if (hour < 9 || hour > 16) {
-                                        datewrite2.setError("Please select a time between 9am and 4pm");
-                                        datewrite2.requestFocus();
-                                        return;
-                                    }
 
-                                } catch (ParseException e) {
-                                    datewrite2.setError("Please select a valid date and time");
-                                    datewrite2.requestFocus();
-                                    return;
-                                }
-
-PerforAuth();
+                        PerforAuth();
                                 // If all fields are valid, proceed with booking
                                 // ...
 
@@ -271,7 +231,7 @@ PerforAuth();
 
     private void PerforAuth() {
 
-        if (isNetworkAvailable(this)) {
+
             FirebaseFirestore db = FirebaseFirestore.getInstance();
             FirebaseAuth auth = FirebaseAuth.getInstance();
             String uid = auth.getCurrentUser().getUid();
@@ -280,38 +240,78 @@ PerforAuth();
             String contactauth = contact.getText().toString();
             String reasonauth = reasons.getText().toString();
             String dateinfo = datewrite2.getText().toString();
-            String genderselect=genders.getText().toString();
-            SharedPreferences sharedPreferences = getSharedPreferences("docselect",MODE_PRIVATE);
-            String spec= sharedPreferences.getString("specialization","").toString();
+            String time = timetext.getText().toString();
 
-            DocumentReference neurologistRef =db.collection("appointments").document(uid)
-                    .collection(spec).document("appointment");
+            String genderselect = genders.getText().toString();
+            SharedPreferences sharedPreferences = getSharedPreferences("docselect", MODE_PRIVATE);
+            String spec = sharedPreferences.getString("specialization", "").toString();
 
-            Appointment appointment = new Appointment(nameauth, ageauth, contactauth, reasonauth, dateinfo,genderselect);
+            DocumentReference docRef2 = db.collection("bookingtime")
+                    .document(spec)
+                    .collection(dateinfo)
+                    .document("timeSlots")
+                    .collection("time")
+                    .document(time);
 
-            neurologistRef.set(appointment).addOnSuccessListener(new OnSuccessListener<Void>() {
+            docRef2.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                 @Override
-                public void onSuccess(Void aVoid) {
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            // Document already exists, handle accordingly
+                            new SweetAlertDialog(booking.this)
+                                    .setTitleText("This time slot is already booked.")
+                                    .show();
+                        } else {
+                            // Document does not exist, create it
+                            docRef2.set(new HashMap<>()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    // Success message
+                                    new SweetAlertDialog(booking.this)
+                                            .setTitleText("Time Slot Booked!")
+                                            .show();
+                                    val = 1;
+                                }
+                            });
+                        }
+                    } else {
+                        // Error getting document, handle accordingly
+                    }
+                }
+            });
+
+            if (val != 1) {
+                DocumentReference neurologistRef = db.collection("appointments").document(uid)
+                        .collection(spec).document("appointment");
+
+
+                Appointment appointment = new Appointment(nameauth, ageauth, contactauth, reasonauth, dateinfo, time, genderselect);
+
+                neurologistRef.set(appointment).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
 
 
 // 1. Success message
-                    new SweetAlertDialog(booking.this)
-                            .setTitleText("Appointment Booked!")
-                            .show();
+                        new SweetAlertDialog(booking.this)
+                                .setTitleText("Appointment Booked!")
+                                .show();
 
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(getApplicationContext(), "Failed to add appointment", Toast.LENGTH_SHORT).show();
-                }
-            });
-        } else {
-            Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "No internet connection. Please try again later.", Snackbar.LENGTH_LONG);
-            snackbar.show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Failed to add appointment", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } else {
+
+            }
         }
 
-    }
+
 
     public class Appointment {
         private String name;
@@ -319,17 +319,19 @@ PerforAuth();
         private String phone;
         private String reason;
         private String date;
+        private String time;
         private String genderselect;
 
         public Appointment() {}
 
-        public Appointment(String name, int age, String phone, String reason, String date,String gender) {
+        public Appointment(String name, int age, String phone, String reason, String date, String time, String gender) {
             this.name = name;
             this.age = age;
             this.phone = phone;
             this.reason = reason;
             this.date = date;
-            this.genderselect=gender;
+            this.time = time;
+            this.genderselect = gender;
         }
 
         public String getName() {
@@ -371,14 +373,24 @@ PerforAuth();
         public String getDate() {
             return date;
         }
-        public void setGender(String gender) {
-            this.genderselect = gender;
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
         }
 
         public String getGender() {
             return genderselect;
         }
+
+        public void setGender(String gender) {
+            this.genderselect = gender;
+        }
     }
 
-
 }
+
+
