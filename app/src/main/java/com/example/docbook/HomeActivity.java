da3e6acdde946fe1dda2859332fdb3099a852c84
Vinayak Prakash <vinayakprakash2121@gmail.com>
2023-03-08 20:46:11 +0530

    girr

--- app/src/main/java/com/example/docbook/HomeActivity.java
+++ app/src/main/java/com/example/docbook/HomeActivity.java
@@ -6,20 +6,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
-import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -27,10 +20,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 public class HomeActivity extends AppCompatActivity {
 
-DrawerLayout drawerLayout;
-NavigationView navigationView;
-Toolbar toolbar;
-ActionBarDrawerToggle actionBarDrawerToggle;
 
 
     CardView lgout, appointment, store, cart, abouts, article;
@@ -39,32 +28,17 @@ ActionBarDrawerToggle actionBarDrawerToggle;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        lgout=findViewById(R.id.logout);
-        appointment=findViewById(R.id.doctor);
-        store=findViewById(R.id.medicine);
-        cart=findViewById(R.id.order);
-        abouts=findViewById(R.id.about);
-        article=findViewById(R.id.articles);
-        drawerLayout=findViewById(R.id.drawer_layout);
-        navigationView=findViewById(R.id.navigationview);
-        actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.menu_Open,R.string.clos_menue);
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch(item.getItemId()){
-                    case R.id.nav_home:
-                        Toast.makeText(HomeActivity.this, "set", Toast.LENGTH_SHORT).show();
-                        drawerLayout.closeDrawer(GravityCompat.START);
-                }
-                return true;
-            }
-        });
-        SharedPreferences sharedPreferences =getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
-        String mail= sharedPreferences.getString("mail","").toString();
-        String password=sharedPreferences.getString("password","").toString();
+        lgout = findViewById(R.id.logout);
+        appointment = findViewById(R.id.doctor);
+        store = findViewById(R.id.medicine);
+        cart = findViewById(R.id.order);
+        abouts = findViewById(R.id.about);
+        article = findViewById(R.id.articles);
+
+
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
+        String mail = sharedPreferences.getString("mail", "").toString();
+        String password = sharedPreferences.getString("password", "").toString();
         // Inside your authentication method (e.g. email/password authentication)
         FirebaseAuth.getInstance().signInWithEmailAndPassword(mail, password)
                 .addOnCompleteListener(task -> {
@@ -96,7 +70,7 @@ ActionBarDrawerToggle actionBarDrawerToggle;
         lgout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SharedPreferences.Editor editor=sharedPreferences.edit();
+                SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.clear();
                 editor.apply();
                 // When the user clicks the logout button
@@ -107,38 +81,38 @@ ActionBarDrawerToggle actionBarDrawerToggle;
                 SharedPreferences.Editor editors = prefs.edit();
                 editors.putBoolean("toast_displayed", false);
                 editors.apply();
-                startActivity(new Intent(HomeActivity.this,MainActivity.class));
+                startActivity(new Intent(HomeActivity.this, MainActivity.class));
 
             }
         });
         appointment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(HomeActivity.this,AppointmentActivity.class));
+                startActivity(new Intent(HomeActivity.this, AppointmentActivity.class));
             }
         });
         store.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(HomeActivity.this,MedicineActivity.class));
+                startActivity(new Intent(HomeActivity.this, MedicineActivity.class));
             }
         });
         cart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(HomeActivity.this,CartActivity.class));
+                startActivity(new Intent(HomeActivity.this, CartActivity.class));
             }
         });
         abouts.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(HomeActivity.this,AboutActivity.class));
+                startActivity(new Intent(HomeActivity.this, AboutActivity.class));
             }
         });
         article.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(HomeActivity.this,ImageSliderActvity.class));
+                startActivity(new Intent(HomeActivity.this, ImageSliderActvity.class));
             }
         });
 
@@ -156,18 +130,11 @@ ActionBarDrawerToggle actionBarDrawerToggle;
 
                         finishAffinity(); // Close all activities
                         System.exit(0); // Exit the app
-                       HomeActivity.super.onBackPressed();
+                        HomeActivity.super.onBackPressed();
                     }
                 })
                 .setNegativeButton("No", null)
                 .show();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
