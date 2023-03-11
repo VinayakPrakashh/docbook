1af3a8c24275fa8731090298a659f66aa7e6606a
Vinayak Prakash <vinayakprakash2121@gmail.com>
2023-03-11 17:02:33 +0530

    girr

---
+++ app/src/main/java/com/example/docbook/DoctorPatientsActivity.java
@@ -0,0 +1,91 @@
+package com.example.docbook;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+public class DoctorPatientsActivity extends AppCompatActivity {
+    Button storenav;
+    SweetAlertDialog sDialog;
+    private RecyclerView recyclerView;
+    private PatientAdapter patientadapter;
+    private List<PatientAdapter.Product> productList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_patients);
+        sDialog = new SweetAlertDialog(DoctorPatientsActivity.this);
+        storenav=findViewById(R.id.store);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        productList = new ArrayList<>();
+        patientadapter = new PatientAdapter(this, productList);
+        recyclerView.setAdapter(patientadapter);
+        recyclerView.addItemDecoration(new CartItemDecoration(16));
+
+        Dialog dialog = new Dialog(DoctorPatientsActivity.this);
+        dialog.setContentView(R.layout.loading_dialog);
+        dialog.setCancelable(false);
+        dialog.show();
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+        String specialization= sharedPreferences.getString("specialization","").toString();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference doctorsRef = db.collection("doctor");
+
+
+
+
+        DocumentReference doctorDocRef = doctorsRef.document(specialization);
+
+        CollectionReference patientRef = doctorDocRef.collection("patient");
+
+        patientRef.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot querySnapshot) {
+                for (QueryDocumentSnapshot documentSnapshot : querySnapshot) {
+                    String patientName = documentSnapshot.getString("name");
+                    String pnumber = documentSnapshot.getString("pnumber");
+                    PatientAdapter.Product product = new PatientAdapter.Product(patientName,pnumber);
+                    productList.add(product);
+                    patientadapter.notifyDataSetChanged();
+                    // Do something with patientName and patientPhone
+                }
+            }
+        });
+
+
+
+
+        dialog.dismiss();
+        storenav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DoctorPatientsActivity.this,MedicineActivity.class));
+            }
+        });
+
+    }
+    public void onBackPressed(){
+        startActivity(new Intent(DoctorPatientsActivity.this,DoctorHomeActivity.class));
+    }
+}
