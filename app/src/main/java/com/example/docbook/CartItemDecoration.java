376d3059bf4d59ceaf02f0306a74cea768952412
Vinayak Prakash <vinayakprakash2121@gmail.com>
2023-03-11 11:18:00 +0530

    girr

---
+++ app/src/main/java/com/example/docbook/CartItemDecoration.java
@@ -0,0 +1,21 @@
+package com.example.docbook;
+
+import android.graphics.Rect;
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CartItemDecoration extends RecyclerView.ItemDecoration {
+    private final int verticalSpaceHeight;
+
+    public CartItemDecoration(int verticalSpaceHeight) {
+        this.verticalSpaceHeight = verticalSpaceHeight;
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        super.getItemOffsets(outRect, view, parent, state);
+        outRect.top = verticalSpaceHeight;
+        outRect.bottom = verticalSpaceHeight;
+    }
+}
