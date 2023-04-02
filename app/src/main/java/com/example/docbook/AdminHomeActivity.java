9336c677609b619c3e786b5c629140b5cce5e766
Vinayak Prakash <vinayakprakash2121@gmail.com>
2023-04-02 23:15:07 +0530

    girr

--- app/src/main/java/com/example/docbook/AdminHomeActivity.java
+++ app/src/main/java/com/example/docbook/AdminHomeActivity.java
@@ -1,20 +1,27 @@
 package com.example.docbook;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AdminHomeActivity extends AppCompatActivity {
-CardView settings,appointment,profile,dallpatients,ddoctors,daddpatient,orders,allproducts;
+LinearLayout settings,appointment,lgout,abouts,dallpatients,ddoctors,daddpatient,orders,allproducts,allproducts_t,doctors_t,patients_t;
+ConstraintLayout mid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_home);
+        setContentView(R.layout.activity_new_home_admin);
 orders=findViewById(R.id.allorders);
 dallpatients=findViewById(R.id.allpatients);
         daddpatient=findViewById(R.id.addpatient);
@@ -22,6 +29,20 @@ dallpatients=findViewById(R.id.allpatients);
 allproducts=findViewById(R.id.products);
         settings=findViewById(R.id.settings);
         appointment=findViewById(R.id.doctors);
+        mid=findViewById(R.id.orders_mid);
+        allproducts_t=findViewById(R.id.products_top);
+        doctors_t=findViewById(R.id.doctors_top);
+        patients_t=findViewById(R.id.allpatients_top);
+        abouts=findViewById(R.id.about);
+        lgout=findViewById(R.id.logout);
+        allproducts_t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                {
+                    startActivity(new Intent(AdminHomeActivity.this,AdminProductViewActivity.class));
+                }
+            }
+        });
         allproducts.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,10 +58,41 @@ allproducts=findViewById(R.id.products);
                startActivity(new Intent(AdminHomeActivity.this,AdminSettingsActivity.class));
             }
         });
+        lgout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+                // When the user clicks the logout button
+                FirebaseAuth.getInstance().signOut();
+                Context context = view.getContext();
+// Reset the value of toast_displayed to false
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+                SharedPreferences.Editor editors = prefs.edit();
+                editors.putBoolean("toast_displayed", false);
+                editors.apply();
+
+                startActivity(new Intent(AdminHomeActivity.this, MainActivity.class));
+            }
+        });
+        doctors_t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AdminHomeActivity.this,DoctorAppointmentActivity.class));
+            }
+        });
         appointment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(AdminHomeActivity.this,DoctorAppointmentActivity.class));
+            }
+        });
+        abouts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AdminHomeActivity.this,AboutActivity.class));
             }
         });
         orders.setOnClickListener(new View.OnClickListener() {
@@ -50,6 +102,12 @@ allproducts=findViewById(R.id.products);
             }
         });
         dallpatients.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AdminHomeActivity.this, AdminPatientsActivity.class));
+            }
+        });
+        patients_t.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(AdminHomeActivity.this, AdminPatientsActivity.class));
